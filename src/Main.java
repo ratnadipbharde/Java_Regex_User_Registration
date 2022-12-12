c3809567479912970b73a63d8a65ad67bcd1926c
ratnadipbharde <ratnadipbharde@gmail.com>
2022-12-13 00:07:32 +0530

    Added code for first name validation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Regex User Registration");
+        Main main = new Main();
+        System.out.println("Enter Name : ");
+        String name = scanner.next();
+        System.out.println(main.validateFirstName(name));
+    }
+
+    private boolean validateFirstName(String name) {
+        return Pattern.matches("^[A-Z][a-z]{2,}$", name);
     }
 }
