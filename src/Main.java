19bb25c7b4baaf6236225264cfed24f0fdc3cbf2
Ratnadip Bharde <ratnadipbharde@gmail.com>
2022-12-18 15:20:33 +0530

    Added code for validate all Email sample

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -7,18 +9,28 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Regex User Registration");
         Main main = new Main();
-        System.out.println("Enter First Name and Last Name : ");
-        String firstNameAndLastName = scanner.nextLine();
-        System.out.println(main.validateFirstName(firstNameAndLastName));
-        System.out.println("Enter email Id : ");
-        String email = scanner.nextLine();
-        System.out.println(main.validateEmail(email));
-        System.out.println("Enter Mobile Number : ");
-        String mobileNumber = scanner.nextLine();
-        System.out.println(main.validateMobileNumber(mobileNumber));
-        System.out.println("Enter Password : ");
-        String password = scanner.nextLine();
-        System.out.println(main.validatePassword(password));
+//        System.out.println("Enter First Name and Last Name : ");
+//        String firstNameAndLastName = scanner.nextLine();
+//        System.out.println(main.validateFirstName(firstNameAndLastName));
+//        System.out.println("Enter email Id : ");
+//        String email = scanner.nextLine();
+//        System.out.println(main.validateEmail(email));
+//        System.out.println("Enter Mobile Number : ");
+//        String mobileNumber = scanner.nextLine();
+//        System.out.println(main.validateMobileNumber(mobileNumber));
+//        System.out.println("Enter Password : ");
+//        String password = scanner.nextLine();
+//        System.out.println(main.validatePassword(password));
+        System.out.println("email partern");
+        System.out.println(main.validateEmail("abc@yahoo.com"));
+        System.out.println(main.validateEmail("abc-100@yahoo.com"));
+        System.out.println(main.validateEmail("abc111@abc.com"));
+        System.out.println(main.validateEmail("abc-100@abc.net"));
+        System.out.println(main.validateEmail("abc.100@abc.com.au"));
+        System.out.println(main.validateEmail("abc@1.com"));
+        System.out.println(main.validateEmail("abc@gmail.com.com"));
+        System.out.println(main.validateEmail("abc+100@gmail.com"));
+
     }
 
     private boolean validatePassword(String password) {
@@ -30,7 +42,7 @@ public class Main {
     }
 
     private boolean validateEmail(String email) {
-        return Pattern.matches("^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\. [a-zA-Z0-9-]+)*$", email);
+        return Pattern.matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", email);
     }
 
     private boolean validateFirstName(String firstNameAndLastName) {
